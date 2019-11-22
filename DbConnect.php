0ed05ded5fe172f1a276a0620d633702b92daaaa
Quadruple <atamanatik@hotmail.com>
2019-11-23 01:13:05 +0300

    Database Connection File

---
+++ DbConnect.php
@@ -0,0 +1,19 @@
+<?php
+
+class DbConnect{
+  private $con;
+
+  function __construct() {}
+
+  function connect() {
+    include_once dirname(__FILE__).'\Constants.php'
+    this->con = new mysqli($DB_SERVER, $DB_USER, $DB_PASS, $DB_NAME);
+
+    if(mysqli_connect_errno()) {
+      echo "Failed to connect to the database.".mysqli_connect_error();
+    }
+
+    return $this->con;
+  }
+}
+?>
