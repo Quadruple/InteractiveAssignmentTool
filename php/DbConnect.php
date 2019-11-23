4077d49ca4f4581d5a5af835459f3dcdec295f9e
Quadruple <atamanatik@hotmail.com>
2019-11-23 19:04:37 +0300

    Another missing column

--- php/DbConnect.php
+++ php/DbConnect.php
@@ -6,7 +6,7 @@ class DbConnect{
   function __construct() {}
 
   function connect() {
-    include_once dirname(__FILE__).'\Constants.php'
+    include_once dirname(__FILE__).'\Constants.php';
     $this->con = new mysqli($DB_SERVER, $DB_USER, $DB_PASS, $DB_NAME);
 
     if(mysqli_connect_errno()) {
