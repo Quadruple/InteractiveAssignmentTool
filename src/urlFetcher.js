3f6122b1533fb71dc5805f8981e534d069575c88
atakanataman <atakanataman@sabanciuniv.edu>
2020-03-31 17:38:58 +0300

    Getting Instructors of a Course is implemented

--- src/urlFetcher.js
+++ src/urlFetcher.js
@@ -15,9 +15,44 @@ function FetchClasses(text)
       var indexOfBeginning = line[i].indexOf(">", line[i].indexOf(">") + 1);
       var temp = line[i].indexOf("<", line[i].indexOf("<") + 1);
       var indexOfEnd = line[i].indexOf("<", temp + 1);
-      //console.log(line[i]);
-      //console.log(indexOfBeginning, indexOfEnd);
       console.log(line[i].substring(indexOfBeginning + 1, indexOfEnd));
+    }
+  }
+}
+
+function fetchInstructorsOfAClass(text, coursename)
+{
+  var line = text.split("\n");
+  var arrayLength = line.length;
+
+  var lineNumOfCourse = 0;
+
+  for(var i = 0; i < arrayLength; i++)
+  {
+    if(line[i].includes("<th class=\"ddlabel\" scope=\"row\""))
+    {
+      var indexOfBeginning = line[i].indexOf(">", line[i].indexOf(">") + 1);
+      var temp = line[i].indexOf("<", line[i].indexOf("<") + 1);
+      var indexOfEnd = line[i].indexOf("<", temp + 1);
+      if (line[i].substring(indexOfBeginning + 1, indexOfEnd).includes(coursename))
+      {
+        lineNumOfCourse = i;
+        break;
+      }
+    }
+  }
+
+  for(var i = lineNumOfCourse + 1; i < arrayLength; i++)
+  {
+    if(line[i].includes("<a href=\"mailto:"))
+    {
+      var matchGroup = line[i].match(/target\="(.*?)"/g);
+      for(var i = 0; i < matchGroup.length; i++)
+      {
+        var instructorName = matchGroup[i].match(/(?<=\")(.*?)(?=\")/g);
+        console.log(instructorName)
+      }
+      break;
     }
   }
 }
@@ -25,11 +60,11 @@ function FetchClasses(text)
 fetch(classesUrl).then(function(response) {
     return response.text();
   }).then(function(body) {
-    //console.log(body);
     textToFetch = body;
   }).then(function(){
     //console.log(textToFetch);
   }).then(function(){
-    //console.log(typeof textToFetch);
     FetchClasses(textToFetch);
+  }).then(function(){
+    fetchInstructorsOfAClass(textToFetch, "VA 455");
   });
